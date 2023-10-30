874d77c0cdf71dbd8c86c98a753a486400be26e8
Armando Monjane <armando.f.monjane@gmail.com>
2023-10-30 16:50:34 +0200

    docs: Added docs to LoggedUser decorator

--- src/decorators/LoggedUser.ts
+++ src/decorators/LoggedUser.ts
@@ -1,11 +1,14 @@
 import { IRequest } from '@/infrastructure/interfaces/IRequest';
 import { createParamDecorator } from 'routing-controllers';
 
-export function LoggedUser() {
-	return createParamDecorator({
-		value: (action) => {
-			const { loggedUser } = action.request as IRequest;
-			return loggedUser;
-		},
-	});
-}
+
+/**
+ * A decorator that retrieves the logged user from the request object.
+ * @returns paramDecorator - A param decorator that retrieves the logged user from the request object.
+ */
+export const LoggedUser = () => createParamDecorator({
+	value: (action) => {
+		const { loggedUser } = action.request as IRequest;
+		return loggedUser;
+	},
+});
