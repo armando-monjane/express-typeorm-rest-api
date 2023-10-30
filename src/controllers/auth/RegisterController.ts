4e1e45e4c0c2b13ae474b51095d030d06f6a5ed7
Armando Monjane <armando.f.monjane@gmail.com>
2023-10-30 16:28:14 +0200

    fix: fixed status codes for /register endpoint
    Changed success status code from 200 to 201 (resource Created successfully)
    Changed error status code when e-mail already exists from 500 to 409 (Conflict)

--- src/controllers/auth/RegisterController.ts
+++ src/controllers/auth/RegisterController.ts
@@ -1,5 +1,5 @@
 import { Service } from 'typedi';
-import { JsonController, Body, Post, UploadedFiles } from 'routing-controllers';
+import { JsonController, Body, Post, UploadedFiles, HttpCode } from 'routing-controllers';
 
 import { OpenAPI } from 'routing-controllers-openapi';
 import { RegisterService } from '@/services/auth/RegisterService';
@@ -15,14 +15,16 @@ import { ILoggedUser } from '@/types/dtos/ILoggedUser';
 export class RegisterController {
 	public constructor(private registerService: RegisterService) { }
 
-    
 	/**
 	 * Registers a new user with the provided registration data and files.
 	 * Receives a multipart/form-data request with the following fields:
 	 * @param {IFile[]} files - The files uploaded during registration.
 	 * @param {RegisterRequest} registerRequest - The registration data.
-	 * @returns {Promise<ILoggedUser>} - A Promise that resolves to the result of the registration.
+	 * @returns {Promise<ILoggedUser>} - A Promise that resolves to the result of the registration with status code 201.
+	 * @throws {BadRequestException} - If the provided data is invalid.
+	 * @throws {ConflictException} - If the provided email is already in use.
 	 */
+	@HttpCode(201)
 	@Post()
 	public async register(
         @UploadedFiles('files') files: IFile[],
